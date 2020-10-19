3484d63f0850be590667ee62bd549764b3253cc3
ryanyogan <ryanyogan@gmail.com>
2020-10-19 13:46:05 -0500

    init gql-gen

---
+++ graph/schema.resolvers.go
@@ -0,0 +1,25 @@
+package graph
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+	"fmt"
+
+	"yogan.dev/meetmeup/graph/generated"
+	"yogan.dev/meetmeup/graph/model"
+)
+
+func (r *queryResolver) Meetups(ctx context.Context) ([]*model.Meetup, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
+// Query returns generated.QueryResolver implementation.
+func (r *Resolver) Query() generated.QueryResolver {
+	return &queryResolver{r}
+}
+
+type queryResolver struct {
+	*Resolver
+}
